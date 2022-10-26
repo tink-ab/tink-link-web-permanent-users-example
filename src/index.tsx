bd857eb8c8239123cab5c6a2a233c029ccb67a8e
gdbroman <gustaf.broman@tink.com>
2022-10-26 15:19:58 +0200

    fix: switch to routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import { AddCredentials } from './AddCredentials';
 import { TinkLinkCallback } from './TinkLinkCallback';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement );
+
+root.render(
   <Router>
-    <Switch>
-      <Route path="/callback">
-        <TinkLinkCallback />
+    <Routes>
+      <Route path="/callback" element={<TinkLinkCallback />} />
+      <Route path="/" element={<AddCredentials />}>
       </Route>
-      <Route path="/">
-        <AddCredentials />
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById('root')
+    </Routes>
+  </Router>
 );
